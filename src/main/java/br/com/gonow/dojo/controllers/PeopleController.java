d043257c7280e395bb7ec756a76d8a159b004fe3
Fernando Brule <fernandobrule@ubuntu.ubuntu-domain>
2011-10-13 19:21:20 -0300

    Dojo #1 - falta el show/id

---
+++ src/main/java/br/com/gonow/dojo/controllers/PeopleController.java
@@ -0,0 +1,26 @@
+package br.com.gonow.dojo.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Resource;
+
+@Resource
+public class PeopleController {
+
+	// @Get , by default is @Get
+	@Path("/people/")
+	public List<String> list() {
+		final List<String> lista = new ArrayList<String>();
+
+		return lista;
+	}
+
+	@Get
+	@Path("/show")
+	public String show() {
+		return "People: ";
+	}
+}
---
+++ src/test/java/br/com/gonow/dojo/controllers/PeopleControllerTest.java
@@ -0,0 +1,22 @@
+package br.com.gonow.dojo.controllers;
+
+import org.japybara.WebTestCase;
+import org.junit.Test;
+
+public class PeopleControllerTest extends WebTestCase {
+	
+	@Test
+	public void shouldGetAListOfPeople() throws Exception {
+		 visit("/people/");
+	     assertHasContent("List");
+	}
+	
+	@Test
+	public void shouldGetPeopleById() throws Exception {
+		 visit("/show");
+		 System.out.println(getContent());
+	     assertHasContent("People: 15");
+	}
+	
+
+}
